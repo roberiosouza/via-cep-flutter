b6fa5b7508bcb54c7ea2990a8f0633889ebb5d0f
Robério <rroberiosouza@gmail.com>
2024-05-28 10:34:58 -0300

    Adicionado TextField para que a consulta se torne dinamica. Também foi adicioando Text para apresentar os resultados da busca.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,11 +12,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _cepController = TextEditingController();
+  String _logradouro = "";
+  String _bairro = "";
+  String _uf = "";
+
   _recuperarCep() async {
-    var url = Uri.parse("https://viacep.com.br/ws/24210410/json/");
+    var url =
+        Uri.parse("https://viacep.com.br/ws/${_cepController.text}/json/");
     http.Response respose;
     respose = await http.get(url);
     Map<String, dynamic> dados = json.decode(respose.body);
+
+    setState(() {
+      _logradouro = dados["logradouro"];
+      _bairro = dados["bairro"];
+      _uf = dados["uf"];
+    });
   }
 
   @override
@@ -26,7 +39,40 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-          ElevatedButton(onPressed: _recuperarCep, child: Text("Clique Aqui"))
+          Padding(
+            padding: EdgeInsets.fromLTRB(50, 80, 50, 0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: "Digite aqui um CEP"),
+              controller: _cepController,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+                onPressed: _recuperarCep, child: Text("Clique Aqui")),
+          ),
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Text("Logradouro: ${_logradouro}"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Text("Bairro: ${_bairro}"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Text("UF: ${_uf}"),
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
